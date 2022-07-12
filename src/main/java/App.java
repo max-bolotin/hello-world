b758132f2364ab4bd0f7c3687d98203c22d738af
max-bolotin <realprof.mx@gmail.com>
2022-07-12 21:11:17 +0300

    divided initialisation of variables into two lines

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) {
-        int a= 10, b = 7;
+        int a= 10;
+        int b = 7;
         System.out.println(sum(a, b));
         System.out.println(div(a, b));
     }
